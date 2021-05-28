5bdeef3f451c2f9683297863db7d0a83d81688a6
cnjlv <79581768+cnjlv@users.noreply.github.com>
2021-05-28 10:20:08 +0200

    added different URI

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,6 +1,7 @@
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
+
 const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
 
@@ -12,7 +13,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://cnjlv.github.io/meet/"],
+  redirect_uris: ["https://github.com/cnjlv/meet/tree/Task-4.2"],
   javascript_origins: ["https://cnjlv.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
@@ -23,7 +24,7 @@ const oAuth2Client = new google.auth.OAuth2(
 );
 
 module.exports.getAuthURL = async () => {
- 
+
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
