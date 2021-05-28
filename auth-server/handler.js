9dcf3cb3d190513bddf3754c8f453ed0d9355d4a
cnjlv <79581768+cnjlv@users.noreply.github.com>
2021-05-28 10:21:18 +0200

    Revert "added different URI"
    This reverts commit 5bdeef3f451c2f9683297863db7d0a83d81688a6.

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,7 +1,6 @@
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
-
 const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
 
@@ -13,7 +12,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://github.com/cnjlv/meet/tree/Task-4.2"],
+  redirect_uris: ["https://cnjlv.github.io/meet/"],
   javascript_origins: ["https://cnjlv.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
@@ -24,7 +23,7 @@ const oAuth2Client = new google.auth.OAuth2(
 );
 
 module.exports.getAuthURL = async () => {
-
+ 
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
