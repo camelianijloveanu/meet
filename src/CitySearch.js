ea3e92621d164512afc427f009716b7c6a783a23
cnjlv <79581768+cnjlv@users.noreply.github.com>
2021-06-06 12:09:15 +0200

    integration testing user can select a city

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,13 +3,17 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
     state = {
         query: '',
-        suggestions: []
+        suggestions: [],
+        showSuggestions: undefined
       };
 
       handleItemClicked = (suggestion) => {
         this.setState({
-          query: suggestion
+          query: suggestion,
+          showSuggestions: false
         });
+      
+        this.props.updateEvents(suggestion);
       }
       
       handleInputChanged = (event) => {
@@ -31,16 +35,17 @@ class CitySearch extends Component {
         className="city"
         value={this.state.query}
         onChange={this.handleInputChanged}
+        onFocus={() => { this.setState({ showSuggestions: true }) }}
       />
-    <ul className="suggestions">
+    <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
     {this.state.suggestions.map((suggestion) => (
         <li
         key={suggestion}
         onClick={() => this.handleItemClicked(suggestion)}
         >{suggestion}</li>
     ))}
-    <li>
-        <b>See all cities</b>
+    <li onClick={() => this.handleItemClicked("all")}>
+      <b>See all cities</b>
     </li>
     </ul>
       </div>
